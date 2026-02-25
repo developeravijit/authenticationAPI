bb643b4950ab4cc6b8419fd8cf30ae52a5022926
developeravijit <roysidharth1996@gmail.com>
2026-02-25 23:44:27 +0530

    accesable for everyone

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 // mongoose.connect("mongodb://127.0.0.1:27017/authDB");
 const connectDB = async () => {

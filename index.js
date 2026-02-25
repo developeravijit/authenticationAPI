0cbee1386dba7196ac9b8a4ee297c434b8d0e5ad
developeravijit <roysidharth1996@gmail.com>
2026-02-25 16:14:08 +0530

    fixed major bug mongodb atlas

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/authDB");
+// mongoose.connect("mongodb://127.0.0.1:27017/authDB");
+mongoose.connect(process.env.MONGO_URI);
 try {
   console.log("MongoDB Connected");
 } catch (err) {
